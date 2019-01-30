00621d97d5459e0f2457cfe8a50aff65cf1173d6
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-01-30 13:16:45 -0800

    Update main.cpp

--- D01 - C++ Basics 2/ex00/main.cpp
+++ D01 - C++ Basics 2/ex00/main.cpp
@@ -1,5 +1,13 @@
+// To complie the exercise: clang++ -Wall -Wextra -Werror Pony.cpp main.cpp
+// Output:
+// Karl is unicorn
+// Piter is pony
+
 #include "Pony.hpp"
 #include <iostream>
+// <cstdlib> (stdlib.h)
+// This header defines several general purpose functions, including dynamic memory management, random number generation, 
+// communication with the environment, integer arithmetics, searching, sorting and converting.
 #include <cstdlib>
 
 static void	ponyOnTheHeap( void )
@@ -22,7 +30,7 @@ int		main( void )
 {
 	ponyOnTheHeap();
 	ponyOnTheStack();
-
+	// int system (const char* command);		Invokes the command processor to execute a command.
 	system("leaks -q a.out");
 	return 0;
 }
