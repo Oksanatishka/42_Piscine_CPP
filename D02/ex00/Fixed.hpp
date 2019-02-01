b88a470122e898e98ace08b0418d64db3a1862eb
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-01-31 23:52:19 -0800

    Update Fixed.hpp

--- D02/ex00/Fixed.hpp
+++ D02/ex00/Fixed.hpp
@@ -3,22 +3,32 @@
 
 #include <iostream>
 
-class Fixed {
+// a canonical class to represent fixed point numbers :
+class Fixed
+{
+// Public members :
+  public:
+	// 1. A default constructor that initializes the fixed point value to 0.
+	Fixed(void);
+	// 3. A copy constructor.
+	Fixed(Fixed const &toCopy);
+	// 2. A destructor.
+	~Fixed(void);
 
-public:
-	Fixed( void );
-	Fixed( Fixed const & toCopy );
-	~Fixed( void );
+	// 4. An assignation operator overload.
+	Fixed &operator=(Fixed const &toCopy);
 
-	Fixed &			operator=( Fixed const & toCopy );
+	// 5. A member function int getRawBits( void ) const; that returns the raw value of the fixed point value.
+	int getRawBits(void) const;
+	// 6. A member function void setRawBits( int const raw ); that sets the raw value of the fixed point value.
+	void setRawBits(int const raw);
 
-	int				getRawBits( void ) const;
-	void			setRawBits( int const raw );
-
-private:
-	int					_rawBits;
-	static const int	_fractionalBits = 8;
-
+// Private members :
+  private:
+	// 7. An integer to store the fixed point value.
+	int _rawBits;
+	// 8. A static constant integer to store the number of fractional bits. This con- stant will always be the litteral 8.
+	static const int _fractionalBits = 8;
 };
 
 #endif
