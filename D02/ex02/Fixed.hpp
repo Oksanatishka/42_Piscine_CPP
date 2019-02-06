b20f9a9b1a74324bbee871c69da060b8acdadc12
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-02-05 16:39:49 -0800

    Update Fixed.hpp

--- D02/ex02/Fixed.hpp
+++ D02/ex02/Fixed.hpp
@@ -14,16 +14,19 @@ public:
 	~Fixed( void );
 
 	Fixed			&operator=( const Fixed &toEquate );
+	// operator overloads (Four arithmetic operators)
 	Fixed			operator+( const Fixed &toAdd ) const;
 	Fixed			operator-( const Fixed &toSubtract ) const;
 	Fixed			operator*( const Fixed &toMultiply ) const;
 	Fixed			operator/( const Fixed &toDivide ) const;
-
+	
+	// operator overloads (The pre-increment, post-increment, pre-decrement and post-decrement operators)
 	Fixed			&operator++( void );
 	Fixed			operator++( int );
 	Fixed			&operator--( void );
 	Fixed			operator--( int);
 
+	// operator overloads (Six comparison operators)
 	bool			operator>( const Fixed &toCompare ) const;
 	bool			operator<( const Fixed &toCompare ) const;
 	bool			operator>=( const Fixed &toCompare ) const;
@@ -35,7 +38,10 @@ public:
 	void			setRawBits( const int raw );
 	float			toFloat( void ) const;
 	int				toInt( void ) const;
-
+	
+	// The non-member function min(max) that takes references on two fixed point values and 
+	// returns a reference to the smallest (biggest) value, and an overload that takes references on two constant fixed point values 
+	// and returns a reference to the smallest (biggest) constant value.
 	static const Fixed	&min( const Fixed &f1, const Fixed &f2 );
 	static const Fixed	&max( const Fixed &f1, const Fixed &f2 );
 
