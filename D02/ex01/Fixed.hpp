76b2e24aa1bf28be613960f60dc711e38a997208
Oksana Bibik <Oksanatishka@users.noreply.github.com>
2019-02-05 16:32:49 -0800

    Update Fixed.hpp

--- D02/ex01/Fixed.hpp
+++ D02/ex01/Fixed.hpp
@@ -2,7 +2,7 @@
 #define FIXED_hpp
 
 #include <iostream>
-#include <cmath>
+#include <cmath>	// for roundf f-n
 
 class Fixed {
 
@@ -15,17 +15,18 @@ public:
 
 	Fixed &			operator=( Fixed const & toCopy );
 
-	int				getRawBits( void ) const;
+	int			getRawBits( void ) const;
 	void			setRawBits( const int raw );
-	float			toFloat( void ) const;
-	int				toInt( void ) const;
+	float			toFloat( void ) const;		// converts the fixed point value to a floating point value.
+	int			toInt( void ) const;	// converts the fixed point value to an integer value.
 
 private:
-	int					_val;
+	int			_val;
 	static const int	_fractionalBits = 8;
 
 };
 
+// An overload to the « operator that inserts a floating point representation of the fixed point value into the parameter output stream.
 std::ostream &		operator<<( std::ostream & o, const Fixed & fixed );
 
 #endif
